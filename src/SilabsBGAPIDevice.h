62f0f55e40c3ad94a1b0e2af88ee59ad59265052
Jeff Rowberg <jeff@rowberg.net>
2019-04-21 23:17:35 -0400

    Move RX buffer storage outside of parser/generator class

--- src/SilabsBGAPIDevice.h
+++ src/SilabsBGAPIDevice.h
@@ -13,10 +13,11 @@ class SilabsBGAPIDevice : public StreamDevice
 public:
     SilabsBGAPIDevice(::Stream *arduinoUart)
             : StreamDevice(&stream),
-              parserGenerator(&protocol),
+              parserGenerator(&protocol, rxBuffer, sizeof(rxBuffer)),
               stream(arduinoUart, &parserGenerator, this) { }
     
 protected:
+    uint8_t rxBuffer[256];
     SilabsBGAPIProtocol protocol;
     StreamParserGenerator parserGenerator;
     UartStream stream;
