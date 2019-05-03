301a427f020a06a7f5154fe5089b52d4b6222f75
Jeff Rowberg <jeff@rowberg.net>
2019-05-02 23:29:21 -0400

    Implement draft BGAPI definition search methods

--- src/SilabsBGAPIProtocol.cpp
+++ src/SilabsBGAPIProtocol.cpp
@@ -105,26 +105,69 @@ int8_t SilabsBGAPIProtocol::getPacketFromNameAndArgs(StreamPacket *packet, const
     return 0;
 }
 
-int8_t SilabsBGAPIProtocol::getPacketDefinition(uint16_t index, uint8_t **packetDef)
+int8_t SilabsBGAPIProtocol::getPacketDefinition(uint16_t index, const uint8_t **packetDef)
 {
-    // TODO: THIS
-    return -1;
-}
-
-uint8_t SilabsBGAPIProtocol::getArgumentCount(uint8_t *packetDef)
-{
-    // TODO: THIS
+    // ensure destination pointer is valid
+    if (!packetDef) return -1;
+    
+    // determine correct look-up table and search range, assume commands
+    const uint8_t *search = SilabsBGAPIProtocol::bleProtocolCommandTable;
+    uint16_t max = BLE_IDX_CMD_MAX;
+    uint8_t tableId = 0;
+    if (index > BLE_IDX_CMD_MAX)
+    {
+        // switch to event table
+        search = SilabsBGAPIProtocol::bleProtocolEventTable;
+        index -= (BLE_IDX_CMD_MAX + 1);
+        max = BLE_IDX_EVT_MAX - (BLE_IDX_CMD_MAX + 1);
+        tableId = 1;
+    }
+    
+    // abort if requested packet is out of range
+    if (index >= max) return -2;
+    
+    // jump through [index] definitions to obtain requested entry
+    uint16_t i;
+    for (i = 0; i < index; i++)
+    {
+        if (tableId == 0)
+        {
+            // 4 bytes fixed definition size + [out] command args, [in] response args (8th bit masked)
+            search += 4 + search[2] + (search[3] & 0x7F);
+        }
+        else
+        {
+            // 3 bytes fixed definition size + [in] event args
+            search += 3 + search[2];
+        }
+    }
+    
+    // assign definition pointer and return success
+    *packetDef = search;
     return 0;
 }
 
-uint8_t *SilabsBGAPIProtocol::getFirstArgument(uint8_t *packetDef)
+uint8_t SilabsBGAPIProtocol::getArgumentCount(const uint8_t *packetDef)
 {
-    // TODO: THIS
+    if (packetDef) return packetDef[2];
+    else return 0;
+}
+
+const uint8_t *SilabsBGAPIProtocol::getFirstArgument(const uint8_t *packetDef)
+{
+    if (packetDef) return &packetDef[4];
     return 0;
 }
 
 const uint8_t SilabsBGAPIProtocol::bleProtocolCommandTable[] =
 {
+    /* system_reset (ID=0/0) */
+        /* group_id */      0x00,
+        /* method_id */     0x00,
+        /* outarg_count */  0x01,
+        /* inarg_count */   0x80,
+        /* cmda[0] */       UINT8,
+        
     /* system_hello (ID=0/1) */
         /* group_id */      0x00,
         /* method_id */     0x01,
@@ -138,13 +181,13 @@ const uint8_t SilabsBGAPIProtocol::bleProtocolEventTable[] =
         /* group_id */      0x00,
         /* method_id */     0x00,
         /* inarg_count */   0x07,
-        /* evta[0] */       StreamProtocol::UINT16, /* uint16 major */
-        /* evta[1] */       StreamProtocol::UINT16, /* uint16 minor */
-        /* evta[2] */       StreamProtocol::UINT16, /* uint16 patch */
-        /* evta[3] */       StreamProtocol::UINT16, /* uint16 build */
-        /* evta[4] */       StreamProtocol::UINT16, /* uint16 ll_version */
-        /* evta[5] */       StreamProtocol::UINT8,  /* uint8 protocol_version */
-        /* evta[6] */       StreamProtocol::UINT8,  /* uint8 hw */
+        /* evta[0] */       UINT16, /* uint16 major */
+        /* evta[1] */       UINT16, /* uint16 minor */
+        /* evta[2] */       UINT16, /* uint16 patch */
+        /* evta[3] */       UINT16, /* uint16 build */
+        /* evta[4] */       UINT16, /* uint16 ll_version */
+        /* evta[5] */       UINT8,  /* uint8 protocol_version */
+        /* evta[6] */       UINT8,  /* uint8 hw */
 };
 
 } // namespace Perilib
--- src/SilabsBGAPIProtocol.h
+++ src/SilabsBGAPIProtocol.h
@@ -9,12 +9,11 @@ namespace Perilib
 class SilabsBGAPIProtocol : public StreamProtocol
 {
 public:
-    enum ble_idx_cmd_t {
+    enum ble_idx_t {
+        BLE_IDX_CMD_SYSTEM_RESET,
         BLE_IDX_CMD_SYSTEM_HELLO,
-        BLE_IDX_CMD_MAX
-    };
-    
-    enum ble_idx_evt_t {
+        BLE_IDX_CMD_MAX,
+        
         BLE_IDX_EVT_SYSTEM_BOOT,
         BLE_IDX_EVT_MAX
     };
@@ -23,9 +22,9 @@ public:
     virtual int8_t testPacketComplete(const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator=0, bool isTx=false);
     virtual int8_t getPacketFromBuffer(StreamPacket *packet, const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator=0, bool isTx=false);
     virtual int8_t getPacketFromNameAndArgs(StreamPacket *packet, const char *packetName, StreamParserGenerator *parserGenerator=0, ...);
-    virtual int8_t getPacketDefinition(uint16_t index, uint8_t **packetDef);
-    virtual uint8_t getArgumentCount(uint8_t *packetDef);
-    virtual uint8_t *getFirstArgument(uint8_t *packetDef);
+    virtual int8_t getPacketDefinition(uint16_t index, const uint8_t **packetDef);
+    virtual uint8_t getArgumentCount(const uint8_t *packetDef);
+    virtual const uint8_t *getFirstArgument(const uint8_t *packetDef);
 
 private:
     static const uint8_t bleProtocolCommandTable[];
