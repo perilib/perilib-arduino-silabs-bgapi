55cf1a395a80e52802e0cb032d559568d8f369e2
Jeff Rowberg <jeff@rowberg.net>
2019-05-28 23:52:12 -0400

    Include additional definitions in main library header

--- src/Perilib_Silabs_BGAPI.h
+++ src/Perilib_Silabs_BGAPI.h
@@ -7,8 +7,16 @@
 
 #include "SilabsBGAPIDevice.h"
 #include "SilabsBGAPIDeviceBLE1XX.h"
+#include "SilabsBGAPIDeviceBGM1XX.h"
+#include "SilabsBGAPIDeviceBT121.h"
+#include "SilabsBGAPIDeviceWF121.h"
+#include "SilabsBGAPIDeviceWGM110.h"
 #include "SilabsBGAPIProtocol.h"
 #include "SilabsBGAPIProtocolBLE1XX.h"
+#include "SilabsBGAPIProtocolBGM1XX.h"
+#include "SilabsBGAPIProtocolBT121.h"
+#include "SilabsBGAPIProtocolWF121.h"
+#include "SilabsBGAPIProtocolWGM110.h"
 #include "SilabsBGAPIPacket.h"
 
 #endif /* __PERILIB_SILABS_BGAPI_H__ */
